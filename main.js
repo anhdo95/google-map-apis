686f51d64e60a7456164f9cf19aa477eb1ded2b1
Anh Do <dinhanhdo95@gmail.com>
2020-01-23 22:33:58 +0700

    Showing multiple markers on the map

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 import './scripts.js'
 
 document.onreadystatechange = function() {
-  const hanoiCoords = { lat: 21.0289882, lng: 105.8474674 }
+  const hanoiCoords = { lat: 20.9825469, lng: 105.7944277 }
 
   const map = new google.maps.Map(document.getElementById('map'), {
     center: hanoiCoords,
-    zoom: 18,
+    zoom: 14,
     backgroundColor: 'rgba(0,0,0,.5)',
     controlSize: 25,
   })
 
-  const marker = new google.maps.Marker({
-    position: hanoiCoords,
-    map
+  const locations = [
+    { lat: 20.9941104, lng: 105.8049496 },
+    { lat: 20.9960312, lng: 105.8028946 },
+    { lat: 20.981876, lng: 105.7893306 },
+    { lat: 20.9915358, lng: 105.789563 },
+  ]
+
+  const markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      map,
+      position: location,
+      label: (i + 1).toString()
+    })
   })
 }
