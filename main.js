03201be285f9ee0e9f10cec7cfbc48a69acaeaa2
Anh Do <dinhanhdo95@gmail.com>
2020-01-23 22:15:07 +0700

    Showing a marker

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 import './scripts.js'
 
 document.onreadystatechange = function() {
-  new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 21.0287602, lng: 105.8493465 },
-    zoom: 16
-  });
+  const hanoiCoords = { lat: 21.0289882, lng: 105.8474674 }
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: hanoiCoords,
+    zoom: 18,
+    backgroundColor: 'rgba(0,0,0,.5)',
+    controlSize: 25,
+  })
+
+  const marker = new google.maps.Marker({
+    position: hanoiCoords,
+    map
+  })
 }
